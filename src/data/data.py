1817052ceddf669ca2ccc64835a260b3654ca3aa
Jacopo Gaetani <jacopus.g@gmail.com>
2022-01-10 23:55:26 +0100

    FIXING COMMIT: changed data.py to use all the files, added config file, implemented hydra in training, not working in evaluating, reached 91% accuracy

--- src/data/data.py
+++ src/data/data.py
@@ -5,19 +5,23 @@ import torch
 
 
 def mnist():
-    # exchange with the corrupted mnist dataset
-    # train = torch.randn(50000, 784)
-    # test = torch.randn(10000, 784) 
+    # path = os.path.abspath(os.path.join(os.getcwd(), 'data', 'raw', 'corruptmnist')) # used for debugging
+    path = os.path.abspath(os.path.join(os.getcwd(), '..','..','..', 'data', 'raw', 'corruptmnist'))
 
-    path = os.path.abspath(os.path.join(os.getcwd(), '..','..', 'data', 'raw', 'corruptmnist'))
+    images = []
+    labels = []
+    for i in range(5):
+        file_name = 'train_' + str(i) + '.npz'
+        train_set = np.load(os.path.join(path, file_name))
+        images.append(torch.Tensor(train_set[train_set.files[0]]))
+        labels.append(torch.Tensor(train_set[train_set.files[1]]).type(torch.LongTensor))
 
-    train_0 = np.load(os.path.join(path, 'train_0.npz'))
-    train_1 = np.load(os.path.join(path, 'train_1.npz'))
-    train_2 = np.load(os.path.join(path, 'train_2.npz'))
-    train_3 = np.load(os.path.join(path, 'train_3.npz'))
-    train_4 = np.load(os.path.join(path, 'train_4.npz'))
-    train = train_0
-
-    test = np.load(os.path.join(path, 'test.npz'))
-    return train, test
-
+    train_images = torch.cat(([t for t in images]), dim = 0)
+    train_labels = torch.cat(([t for t in labels]), dim = 0)
+    train_dataset = torch.utils.data.TensorDataset(train_images, train_labels)
+    
+    test_set = np.load(os.path.join(path, 'test.npz'))
+    img = torch.Tensor(test_set[test_set.files[0]])
+    lbs = torch.Tensor(test_set[test_set.files[1]]).type(torch.LongTensor)
+    test_dataset = torch.utils.data.TensorDataset(img, lbs)
+    return train_dataset, test_dataset
